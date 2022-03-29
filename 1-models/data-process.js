f757198654cbb5b8a4aee503eefbb27d33bc319d
Paul Kang <paul@paul-kang.com>
2022-03-29 21:49:01 +0800

    改用 https://chdr-weight.herokuapp.com/ api

--- 1-models/data-process.js
+++ 1-models/data-process.js
@@ -1,6 +1,7 @@
 var isAndroid = kendo.support.mobileOS.android;
 
-var apiSite = (useLocalAPIs)?'http://localhost:5000':'https://hwbmi-api.herokuapp.com/';
+//var apiSite = (useLocalAPIs)?'http://localhost:5000':'https://hwbmi-api.herokuapp.com/';
+var apiSite = (useLocalAPIs)?'http://localhost:5000':'https://chdr-weight.herokuapp.com/';
 console.log(apiSite);
 
 var measurementSource = new kendo.data.DataSource({
